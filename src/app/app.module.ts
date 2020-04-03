fdd1c524b25450f6dff589aeffda497cb064085f
nsedrickm <nsedrick101@gmail.com>
2020-04-03 16:10:29 +0100

    side-nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    NavbarComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule,
